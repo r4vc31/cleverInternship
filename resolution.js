51b08a7790b6bf622e1d65e1eb5e85fa58b8d767
r4vc31 <r4vc31@gmail.com>
2022-12-19 23:45:42 -0500

    modified:   resolution.js

--- resolution.js
+++ resolution.js
@@ -1,16 +1,43 @@
-/*
-    1. Suma de digitos
-*/
+/**
+ * 1. Suma de digitos
+ */
 function digitsSum(inputInt){
-    let suma = 0
-    let numbers = []
-    inputInt.split("").forEach( (digit) => numbers.push(parseInt(digit)))
-    numbers.forEach(number => suma += number)
-    return suma
+    let suma = 0;
+    let numbers = [];
+    inputInt.split("").forEach( (digit) => numbers.push(parseInt(digit)));
+    numbers.forEach(number => suma += number);
+    return suma;
 }
-
 // Casos de ejemplo
-console.log(digitsSum("999"))
-console.log(digitsSum("9184501"))
-console.log(digitsSum("12345"))
+console.log(digitsSum("999"));
+console.log(digitsSum("9184501"));
+console.log(digitsSum("12345"));
 
+/**
+ * 2. Palíndromos
+ */
+function isPalindrome(inputStr){
+    return inputStr == inputStr.split("").reverse().join("");
+}
+// Casos de ejemplo
+console.log(isPalindrome("aabaa"));
+console.log(isPalindrome("abac"));
+console.log(isPalindrome("salas"));
+
+/**
+ * 3. Producto de elementos adyacentes
+ */
+function maxAdjacentProd(inputArray){
+    let max = -Infinity;
+    for (let index = 0; index < inputArray.length - 1; index++) {
+        let adjacentProd = inputArray[index]*inputArray[index+1];
+        if (adjacentProd > max){
+            max = adjacentProd;
+        }
+    }        
+    return max;
+}
+// Casos de ejemplo
+console.log(maxAdjacentProd([3, 6, -2, -5, 7, 3]))
+console.log(maxAdjacentProd([5, 1, 2, 3, 1, 4]))
+console.log(maxAdjacentProd([-23, 4, -3, 8, -12]))
